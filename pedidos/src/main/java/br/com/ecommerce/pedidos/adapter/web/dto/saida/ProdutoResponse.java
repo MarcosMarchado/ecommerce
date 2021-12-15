5b07d74e0c0e0a7d19f97511c4dd261933d06ce4
Marcos Machado <marcoseu1994@gmail.com>
2021-12-14 21:39:22 -0300

    Inserindo a Categoria no response de Produto

--- pedidos/src/main/java/br/com/ecommerce/pedidos/adapter/web/dto/saida/ProdutoResponse.java
+++ pedidos/src/main/java/br/com/ecommerce/pedidos/adapter/web/dto/saida/ProdutoResponse.java
@@ -1,8 +1,12 @@
 package br.com.ecommerce.pedidos.adapter.web.dto.saida;
 
+import br.com.ecommerce.pedidos.adapter.model.Categoria;
 import br.com.ecommerce.pedidos.adapter.model.Produto;
 import org.springframework.data.domain.Page;
-//TODO: Colocar na resposta o id e nome da Categoria de cada Produto
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProdutoResponse {
    private Long id;
 
@@ -10,10 +14,19 @@ public class ProdutoResponse {
 
    private Double preco;
 
+   private List<CategoriaResponse> categorias;
+
    public ProdutoResponse(Produto produto) {
       this.id = produto.getId();
       this.nome = produto.getNome();
       this.preco = produto.getPreco();
+   }
+
+   public ProdutoResponse(Produto produto, List<Categoria> categorias) {
+      this.id = produto.getId();
+      this.nome = produto.getNome();
+      this.preco = produto.getPreco();
+      this.categorias = categorias.stream().map(CategoriaResponse::new).collect(Collectors.toList());
    }
 
    public Long getId() {
@@ -28,7 +41,14 @@ public class ProdutoResponse {
       return preco;
    }
 
+   public List<CategoriaResponse> getCategorias() {
+      return categorias;
+   }
+
    public static Page<ProdutoResponse> paraResponse (Page<Produto> produtos){
-      return produtos.map(ProdutoResponse::new);
+      return produtos.map(produto -> {
+         var categorias = produto.getCategorias();
+         return new ProdutoResponse(produto, categorias);
+      });
    }
 }
