67e17cd1c5f56600c321d0d83022967370718a0c
Marcos Machado <marcoseu1994@gmail.com>
2022-03-01 13:42:14 -0300

    Finalizando fluxo da aplicação na arquitetura hexagonal

--- pedidos/src/main/java/br/com/ecommerce/pedidos/core/web/dto/PedidoRequest.java
+++ pedidos/src/main/java/br/com/ecommerce/pedidos/core/web/dto/PedidoRequest.java
@@ -1,6 +1,5 @@
 package br.com.ecommerce.pedidos.core.web.dto;
 
-import br.com.ecommerce.pedidos.adapter.anotations.Pedido;
 import lombok.Getter;
 
 import javax.validation.Valid;
@@ -8,7 +7,6 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
-@Pedido
 public class PedidoRequest {
    @NotNull
    private Long idCliente;
