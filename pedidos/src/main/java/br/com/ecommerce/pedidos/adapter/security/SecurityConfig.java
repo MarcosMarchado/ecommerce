cc30561f7bfd0b59c84db3ae0b66a0cff2845148
Marcos Machado <marcoseu1994@gmail.com>
2022-04-15 01:13:37 -0300

    Finalizando implementação de autenticação com Keycloak

--- pedidos/src/main/java/br/com/ecommerce/pedidos/adapter/security/SecurityConfig.java
+++ pedidos/src/main/java/br/com/ecommerce/pedidos/adapter/security/SecurityConfig.java
@@ -3,28 +3,20 @@ package br.com.ecommerce.pedidos.adapter.security;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
-import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-@Configuration
-@EnableWebSecurity(debug = true)
-//@KeycloakConfiguration
+
+@KeycloakConfiguration
 public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+/*    @Autowired
+    public KeycloakClientRequestFactory keycloakClientRequestFactory;*/
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,9 +33,9 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     }
 
     /*@Bean
-    @Override
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public KeycloakRestTemplate keycloakRestTemplate(){
+        return new KeycloakRestTemplate(keycloakClientRequestFactory);
     }*/
 
 
@@ -52,8 +44,10 @@ public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         super.configure(http);
         http
                 .authorizeRequests()
-                .antMatchers("/api/v1/ecommerce/**").hasRole("ADM")
-                //.antMatchers("/api/v1/ecommerce/categorias").hasRole("ADM")
-                .anyRequest().permitAll();
+                .antMatchers("/api/v1/ecommerce/pedidos**").hasRole("ADM")
+                .antMatchers("/api/v1/ecommerce/categorias**").hasRole("USER")
+                .antMatchers("/api/v1/ecommerce/produtos**").hasRole("USER")
+                .anyRequest().permitAll()
+                .and().csrf().disable();
     }
 }
